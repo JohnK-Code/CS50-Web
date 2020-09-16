553f9271f48861c98f40e922f6199483495ae9df
John Kelly <19jk2009@gmail.com>
2020-09-16 15:03:26 +0100

    Work done so far for CS50 Web Course

---
+++ Lecture-2/sequences.py
@@ -0,0 +1,23 @@
+##String = sequance of letters
+#name = "Harry"
+#print(name[0])
+
+##List = mutable values - can be changed
+#names = ["Harry", "Ron", "Hermoine"]
+#names.append("Draco")
+#names.sort()
+#print(names)
+
+##Tuple = immutable values - can't be changed
+#coordinate = (10.0, 20.0)
+#print(coordinate)
+
+##Set = unordered unique values - can't add same value twice
+s = set()
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+print(s)
+
+##Dict = key-value pairs
