c621d5597828cbc4ed210b7c29b84085ddab99e9
John Kelly <19jk2009@gmail.com>
2020-09-26 21:28:20 +0100

    Update settings.py
    Update settings file

--- Lecture-3/lecture3project/lecture3project/settings.py
+++ Lecture-3/lecture3project/lecture3project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*$%7pvohy*)y(6n=#x%bmqrar1w1^gm+rd5@(myn%rn@_-oa2&'
+SECRET_KEY = 'change me'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
